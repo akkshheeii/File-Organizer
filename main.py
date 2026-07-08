d2b085f1bfad0d0bcf69dbc2e2586a4ffa70951b
Akshay A M <akshayajith127@gmail.com>
2026-07-09 01:15:40 +0530

    Refactor file categorization and organization logic

--- main.py
+++ main.py
@@ -20,10 +20,17 @@ categories = {
     ".mp3": "Audio"
 }
 count=0
-img=[]
-doc=[]
-vid=[]
-aud=[]
+Image=[]
+Video=[]
+Audio=[]
+Document=[]
+Archive=[]
+Code=[]
+Application=[]
+Font=[]
+Database=[]
+Other=[]
+
 print("Total Files Found :  ")
 for file in os.listdir(folder):
     count+=1
@@ -33,40 +40,101 @@ for file in os.listdir(folder):
     if os.path.isfile(file_path):
 
         extension = os.path.splitext(file)[1].lower()
+        if extension in [
+                "jpg", "jpeg", "png", "gif", "bmp", "webp",
+                "svg", "ico", "tiff", "heic"
+            ]:
+                Image.append(file)
 
-        if extension in categories:
-            if categories[extension] == "Images":
-                img.append(file)
-            elif categories[extension] == "Documents":
-                doc.append(file)
-            elif categories[extension] == "Videos":
-                vid.append(file)
-            elif categories[extension] == "Audio":
-                aud.append(file)
+        elif extension in [
+                "mp4", "avi", "mov", "mkv", "wmv",
+                "flv", "webm", "mpeg", "3gp"
+            ]:
+                Video.append(file)
 
+        elif extension in [
+                "mp3", "wav", "aac", "flac",
+                "ogg", "m4a", "wma"
+            ]:
+                Audio.append(file)
 
-            target_folder = os.path.join(
+        elif extension in [
+                "pdf", "doc", "docx", "txt", "rtf",
+                "ppt", "pptx", "xls", "xlsx",
+                "csv", "odt", "ods", "odp"
+            ]:
+                Document.append(file)
+
+        elif extension in [
+                "zip", "rar", "7z", "tar",
+                "gz", "bz2", "xz", "iso"
+            ]:
+                Archive.append(file)
+
+        elif extension in [
+                "py", "java", "cpp", "c",
+                "cs", "js", "ts", "html",
+                "css", "php", "rb", "go",
+                "swift", "kt", "sql", "json",
+                "xml", "yml", "yaml", "sh"
+            ]:
+                Code.append(file)
+
+        elif extension in [
+                "exe", "msi", "apk", "ipa",
+                "deb", "rpm", "dmg", "app"
+            ]:
+                Application.append(file)
+
+        elif extension in [
+                "ttf", "otf", "woff", "woff2"
+            ]:
+                Font.append(file)
+
+        elif extension in [
+                "db", "sqlite", "sqlite3"
+            ]:
+                Database.append(file)
+
+        else:
+                Other.append(file)
+        
+        target_folder = os.path.join(
                 folder,
                 categories[extension]
             )
 
-            os.makedirs(target_folder,
+        os.makedirs(target_folder,
                         exist_ok=True)
 
-            shutil.move(
+        shutil.move(
                 file_path,
                 os.path.join(target_folder,
                              file)
             )
 
 print("\n\nOrganized Files : ")
-if img:
-    print("Images: ", img)
-if doc:
-    print("Documents: ", doc)
-if vid:
-    print("Videos: ", vid)
-if aud:
-    print("Audio: ", aud)
-if not img and not doc and not vid and not aud:
+if Image:
+    print("Images : ", Image,'/n/n')
+if Video:
+    print("Video : ", Video,'/n/n')
+if Audio:
+    print("Audio : ", Audio)
+if Document:
+    print("Documents : ", Document)
+if Archive:
+    print("Archive : ", Archive,'/n/n')
+if Code:
+    print("Code : ", Code,'/n/n')
+if Application:
+    print("Application : ", Application)
+if Font:
+    print("Font : ", Font)
+if Database:
+    print("Database : ", Database)
+if Other:
+    print("Other : ", Other)
+
+
+if (len(Image) + len(Video) + len(Audio) + len(Document)+len(Archive)+len(Code)+len(Application)+len(Font)+len(Database)+len(Other))==0:
     print("No Files To Organize !")
